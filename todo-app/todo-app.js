8f7ece14856dbc1233fcd6e2432e63a00ca8b26d
John Booth <jhunter.booth@gmail.com>
2022-04-07 17:27:02 -0600

    finished challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,40 @@
-const p = document.querySelectorAll("p")
-p.forEach(function(p){
-    if(p.textContent.includes("Need")){
-        p.remove()
-    }
+const todos = [{
+    text: "Order cat food",
+    completed: false
+}, {
+    text: "Clean kitchen",
+    completed: true
+}, {
+    text: "Buy food",
+    completed: true
+}, {
+    text: "Do work",
+    completed: false
+}, {
+    text: "Excercise",
+    completed: true
+}]
+
+// const p = document.querySelectorAll("p")
+// p.forEach(function(p){
+//     if(p.textContent.includes("Need")){
+//         p.remove()
+//     }
+// })
+
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+const incompleteTodos = todos.filter(function (todo){
+    return !todo.completed
+})
+
+const summary = document.createElement("h2")
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo){
+    const p = document.createElement("p")
+    p.textContent = todo.text
+    document.querySelector("body").appendChild(p)
 })
--- web-server/notes-app/notes-app.js
+++ web-server/notes-app/notes-app.js
@@ -1,3 +1,15 @@
+const notes = [{
+    title: "my next trip",
+    body: "I would like to go to Spain"
+}, {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better."
+}, {
+    title: "office modification",
+    body: "Get a new seat"
+}]
+
+
 // DOM - Document Object Model
 
 // Query and remove
@@ -5,7 +17,6 @@
 // p.remove()
 
 // Query all and remove
-
 const ps = document.querySelectorAll("p")
 ps.forEach(function(p){
     p.textContent = `*****`
@@ -13,3 +24,7 @@ ps.forEach(function(p){
     // p.remove()
 })
 
+// Add a new element
+const newParagraph = document.createElement("p")
+newParagraph.textContent = "This is a new element from JavaScript"
+document.querySelector("body").appendChild(newParagraph)
